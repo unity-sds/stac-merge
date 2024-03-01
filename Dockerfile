5878ecfb78a33333e256f8a313e70d918c73105b
mike-gangl <gangl@ip-100-65-57-228.jpl.nasa.gov>
2024-03-01 10:57:40 -0800

    cleaned up emrge code to create featureCollection

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,7 @@ COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+run chmod 777 /usr/src/app/*
 
 CMD [ "python", "./stac_merge.py" ]
+ENTRYPOINT [ "python","/usr/src/app/stac_merge.py"]
--- stac_merge.py
+++ stac_merge.py
@@ -3,11 +3,40 @@ import logging
 import os
 import sys
 
-from unity_sds_client.resources.dataset import Dataset
-from unity_sds_client.resources.data_file import DataFile
+
+from pystac import ItemCollection, Catalog, get_stac_version
+from pystac.errors import STACTypeError
+
+
+def read_stac(stac_file):
+    root_catalog = None
+    try:
+      root_catalog = Catalog.from_file(stac_file)
+      id = root_catalog.id
+      items = root_catalog.get_all_items()
+    except STACTypeError as e:
+      pass
+    # attempt to read as a feature collection
+
+    # ItemCollection
+    if root_catalog is None:
+       with open(stac_file, 'r') as f:
+          data = json.load(f)
+          ic = ItemCollection.from_dict(data)
+          try:
+             id = data['features'][0]['collection']
+          except:
+             pass
+       
+          items = ic.items
+    return items
+
+
+#from unity_sds_client.resources.dataset import Dataset
+#from unity_sds_client.resources.data_file import DataFile
 
 # stage_in packages
-from unity_sds_client.resources.collection import Collection
+#from unity_sds_client.resources.collection import Collection
 
 if len(sys.argv) < 2:
     print("stac_merge requires at least 2 arguments:")
@@ -24,12 +53,17 @@ output_location = sys.argv[1]
 if len(stac_files) == 0:
     print("Not enough stac files given, nothing to do")
     sys.exit()
-    
-master_collection = Collection.from_stac(stac_files[0])
 
-for file in stac_files[1:]:
-  temp_collection = Collection.from_stac(file)
-  for ds in temp_collection.datasets:
-    master_collection.add_dataset(ds)
+items = []
+for x in stac_files:
+    stac_items = read_stac(x)
+    items = items + stac_items 
+#master_collection = Collection.from_stac(stac_files[0])
+#
+#for file in stac_files[1:]:
+#  temp_collection = Collection.from_stac(file)
+#  for ds in temp_collection.datasets:
+#    master_collection.add_dataset(d)s
+#Collection.to_stac(master_collection, output_location)
+ItemCollection(items).save_object(dest_href=output_location+'/catalog.json')
 
-Collection.to_stac(master_collection, output_location)
